add083373e4c1a0417c3839aad218a57ff7ea70f
Chaos Spindash <Chaos Spindash@CHAOSINSPIRON>
2024-05-03 10:21:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using DisCatSharp.Entities;
+
+namespace RadiSharp
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var yamlDeserializer = new DeserializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build();
+
+            YamlConfig yamlConfig = yamlDeserializer.Deserialize<YamlConfig>(File.ReadAllText("config.yaml"));
+
+            var Discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = yamlConfig.Token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContent
+            });
+
+            await Discord.ConnectAsync(new DiscordActivity(yamlConfig.ActivityName,yamlConfig.ActivityType),yamlConfig.Status);
+            await Task.Delay(-1);
+        }
+    }
+}
---
+++ YamlConfig.cs
@@ -0,0 +1,12 @@
+﻿using DisCatSharp.Entities;
+
+namespace RadiSharp
+{
+    class YamlConfig
+    {
+        public string Token { get; set; }
+        public ActivityType ActivityType { get; set; }
+        public string ActivityName { get; set; }
+        public UserStatus Status { get; set; }
+    }
+}
