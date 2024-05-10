a16b20d22e22b5f3a37ddfe740e93217345a8466
ChaosSpindash <chaosspindash@proton.me>
2024-05-10 11:07:37 +0200

    Added fields for Guild ID and Lavalink server

--- YamlConfig.cs
+++ YamlConfig.cs
@@ -4,9 +4,13 @@ namespace RadiSharp
 {
     class YamlConfig
     {
-        public string Token { get; set; }
+        public string Token { get; set; } = "";
         public ActivityType ActivityType { get; set; }
-        public string ActivityName { get; set; }
+        public string ActivityName { get; set; } = "";
         public UserStatus Status { get; set; }
+        public ulong GuildId { get; set; }
+        public string LavalinkHost { get; set; } = "";
+        public int LavalinkPort { get; set; }
+        public string LavalinkPass { get; set; } = "";
     }
 }
