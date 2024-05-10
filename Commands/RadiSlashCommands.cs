e5ced3604a8d81ffba874c3978acc0ec0f81b131
ChaosSpindash <chaosspindash@proton.me>
2024-05-10 11:08:17 +0200

    Misc. Slash Commands

---
+++ Commands/RadiSlashCommands.cs
@@ -0,0 +1,21 @@
+﻿using DisCatSharp.ApplicationCommands;
+using DisCatSharp.ApplicationCommands.Attributes;
+using DisCatSharp.ApplicationCommands.Context;
+using DisCatSharp.Entities;
+using DisCatSharp.Lavalink;
+
+namespace RadiSharp.Commands
+{
+    public class RadiSlashCommands : ApplicationCommandsModule
+    {
+        [SlashCommandGroup("util", "Utility commands")]
+        public class CommandGroupUtil : ApplicationCommandsModule
+        {
+            [SlashCommand("ping", "Pong!")]
+            public async Task PingAsync(InteractionContext ctx)
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Pong!"));
+            }
+        }
+    }
+}
